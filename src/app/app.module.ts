dd0624ec203f4ae82f3feb677126bf2e8fba653f
Andrii Kicha <akicha@star.global>
2023-05-19 07:50:47 -0600

    add assignment implementation
    remove solution

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { AssignmentInfoComponent } from './assignment-info/assignment-info.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  imports: [BrowserModule, HttpClientModule],
+  declarations: [AppComponent, AssignmentInfoComponent],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
